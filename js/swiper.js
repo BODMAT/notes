89344099949f0f3041d54489437d09cd258acef7
Bohdan <matula.bohdan@gmail.com>
2024-06-25 16:12:08 +0300

    first commit

---
+++ js/swiper.js
@@ -0,0 +1,7 @@
+import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
+var swiper = new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
